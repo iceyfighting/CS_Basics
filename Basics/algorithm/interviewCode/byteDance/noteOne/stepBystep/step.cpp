3b305eba58aed586fb72eb64e6be65990ef9d701
iceyfighting <pllfighting@163.com>
2020-08-04 00:17:53 +0800

    add node

---
+++ Basics/algorithm/interviewCode/byteDance/noteOne/stepBystep/step.cpp
@@ -0,0 +1,52 @@
+/*
+ *上台阶，一次可以走一步或两步至n步
+ *打印所有可能的上法
+ * */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<vector<int> > result;
+
+class Solution{
+    public:
+        void stepCount(int n, vector<int> temp)
+        {
+            if(n == 0)
+            {
+                result.push_back(temp);
+            }
+            else if(n < 0)
+            {
+                
+            }
+            else 
+            {
+                vector<int> temp1 = temp;
+                temp1.push_back(1);
+                stepCount(n-1, temp1 );
+                
+                vector<int> temp2 = temp;
+                temp2.push_back(2);
+                stepCount(n-2, temp2 );
+            }
+        }
+};
+
+int main()
+{
+    Solution ss;
+    vector<int> tmp;
+    ss.stepCount(3, tmp);
+    for(vector<vector<int> >::iterator iter=result.begin();iter !=result.end();iter++)
+    {
+        vector<int> vec_temp = *iter;
+        for(vector<int>::iterator it = vec_temp.begin();it != vec_temp.end();it++)
+        {
+            cout << *it << " " ;
+        }
+        cout << endl;
+    }
+    //cout<<result;
+
+}
